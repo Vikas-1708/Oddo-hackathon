2476106874ec7e0c3067dc1ba06d726aee457274
Vikas-1708 <moravikas77@gmail.com>
2025-07-12 12:28:56 +0530

    Update App.jsx

--- App.jsx
+++ App.jsx
@@ -1,13 +1,23 @@
 import { useState } from 'react'
 import './App.css'
-import Login from './components/Login'
+import Signup from './assets/components/Signup'
+import Navbar from './assets/components/Navbar'
+import Mainpage from './assets/components/Mainpage'
+import Login from './assets/components/Login'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-    <Login />
+    <Router>
+      <Routes>
+        <Route path="/" element={<Mainpage />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </Router>
     </>
   )
 }
