8046b2bb11406848b18833821e2184b9c0117442
Akshith <akshithkodithyala@gmail.com>
2025-07-12 11:18:02 +0530

    Add files via upload

---
+++ Mainpage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Mainpage.css'
+import Navbar from './Navbar'
+function Mainpage() {
+  return (
+    <div>
+        <Navbar />
+    </div>
+  )
+}
+
+export default Mainpage
