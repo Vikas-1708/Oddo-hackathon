acfd561b47e32cee627ab900aef3b7019bce7bec
Mukesh <puppalamukeshbabu@gmail.com>
2025-07-12 12:29:27 +0530

    Update Login.jsx

--- Login.jsx
+++ Login.jsx
@@ -1,26 +1,42 @@
-import React from 'react'
+import React, { useState } from 'react'
+import axios from 'axios'
 import './Login.css'
+import { Link } from 'react-router-dom'
 
 function Login() {
+  const [form, setForm] = useState({ username: '', password: '' })
+
+  const handleChange = (e) => {
+    setForm({ ...form, [e.target.name]: e.target.value })
+  }
+
+  const handleSubmit = async (e) => {
+    e.preventDefault()
+    try {
+      const res = await axios.post('http://localhost:5000/api/login', form)
+      alert(res.data.message)
+      // You can redirect or store user info here
+    } catch (err) {
+      alert(err.response?.data?.message || 'Login failed')
+    }
+  }
+
   return (
     <div className="login-container">
-      <form className="login-form">
+      <form className="login-form" onSubmit={handleSubmit}>
         <h2>Login</h2>
         <div className="input-group">
-          <label htmlFor="username">Username</label>
-          <input type="text" id="username" name="username" placeholder="Enter your username" />
+          <label htmlFor="username">Username*</label>
+          <input type="text" id="username" name="username" value={form.username} onChange={handleChange} required />
         </div>
         <div className="input-group">
-          <label htmlFor="password">Password</label>
-          <input type="password" id="password" name="password" placeholder="Enter your password" />
+          <label htmlFor="password">Password*</label>
+          <input type="password" id="password" name="password" value={form.password} onChange={handleChange} required />
         </div>
-        <div className="login-actions">
-          <button type="submit" className="login-btn">Login</button>
-          <a href="#" className="forgot-password">Forgot Password?</a>
-        </div>
+        <button type="submit" className="login-btn">Login</button>
         <div className="signup-link">
-          <span>New here?</span>
-          <a href="#">Sign Up</a>
+          <span>Don't have an account?</span>
+          <Link to="/">Signup</Link>
         </div>
       </form>
     </div>
